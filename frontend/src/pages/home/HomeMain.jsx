27710b33131a37f7dd5069f5cfdef0b4293bcdee
RatheeshCoder <rathee.r.ad.2021@snsce.ac.in>
2024-01-21 18:59:07 +0530

    home page design

--- frontend/src/pages/home/HomeMain.jsx
+++ frontend/src/pages/home/HomeMain.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import HeroHome from '../../components/Home/HeroHome'
 import AchievementsSection from '../../components/Home/Achievement'
+import FeatureMain from '../../components/Home/FeatureMain'
+import ServiesHome from '../../components/Home/Servies'
+import AboutSection from '../../components/Home/AboutUs'
 const HomeMain = () => {
   return (
     <div>
       <HeroHome/>
       <AchievementsSection/>
+      <AboutSection/>
+      <FeatureMain/>
+      <ServiesHome/>
+     
     </div>
   )
 }
